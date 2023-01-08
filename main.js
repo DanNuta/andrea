a329f8b61d186c69a3d9c65ac0aaa03062a8a748
Dan <dany.condurari@gmail.com>
2023-01-08 10:07:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// const galeyParent = document.querySelector("[data-galery]");
+
+// const images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png"];
+
+
+
+
+
+// images.forEach(item => {
+//     const img = document.createElement("img");
+//     img.src = item;
+//     img.classList.add("img");
+
+//     galeyParent.appendChild(img)
+// })
