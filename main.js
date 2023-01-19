3a29c2bfbca2251e120717730bb9d032d2c0b65a
Dan <dany.condurari@gmail.com>
2023-01-19 05:58:52 +0200

    create about section

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-// const galeyParent = document.querySelector("[data-galery]");
 
-// const images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png"];
+const divGalery = document.querySelector(".gallery_shr_img");
+const images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png"];
+const sliderLength = images.length-1;
+
+let counter = 0;
+
+images.forEach(item => {
+    const randomElement = Math.floor(Math.random()*10);
+    const img = document.createElement("img");
+    img.src = item;
+    img.classList.add("img_shr");
+    divGalery.append(img)
+})
+
+const gallery = document.querySelectorAll(".img_shr");
+
+
+setInterval(function(){
+    if(counter > sliderLength){
+        counter = 0;
+    }else{
+        const activeClassGalery = document.querySelector(".active");
+        activeClassGalery?.classList.remove("active");
+        gallery[counter].classList.add("active");
+        counter++
+    }
+}, 1000)
 
 
 
-
-
-// images.forEach(item => {
-//     const img = document.createElement("img");
-//     img.src = item;
-//     img.classList.add("img");
-
-//     galeyParent.appendChild(img)
-// })
